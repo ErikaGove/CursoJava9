ec31e54800d4bd7be7b934b50f67926e70f55cbc
Erika Gonzalez <erikagov0926@gmail.com>
2025-02-12 14:11:02 -0600

    importacion de boostrap

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,7 @@
 import "/src/css/styles.scss" /* Al importAR EL ARCHIVO SASSS, importas todo lo que importa el archivo sass*/
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
+
 
 import { insertMainHeader } from "/src/modules/header/header";
 import { insertMainFooter } from "/src/modules/footer/footer";
--- src/pages/about-us/about.js
+++ src/pages/about-us/about.js
@@ -1,5 +1,8 @@
 import { insertMainHeader } from "/src/modules/header/header.js";
 import { insertMainFooter } from "/src/modules/footer/footer.js";
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
 
 insertMainHeader();
 insertMainFooter();
